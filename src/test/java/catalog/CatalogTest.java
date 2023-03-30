e5b265167eff5ab60de835698269a87b71509b00
ValeriaShatila <123481670+ValeriaShatila@users.noreply.github.com>
2023-03-30 19:05:16 +0300

    add log INFO

--- src/test/java/catalog/CatalogTest.java
+++ src/test/java/catalog/CatalogTest.java
@@ -6,10 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import org.testng.log4testng.Logger;
+import org.testng.annotations.*;
 
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -19,9 +16,9 @@ public class CatalogTest {
     private static WebDriver driver;
     private static final String PRODUCT_NAME_PATTERN = "//span[@class='catalog-navigation-classifier__item-title-wrapper' and text()='%s']";
     private static Properties properties;
-    public static Logger log = Logger.getLogger(CatalogTest.class);
+    protected static Logger logger = catalog.Logger.getInstance();
 
-    @BeforeMethod
+    @BeforeTest
     public void init() {
         BasicConfigurator.configure();
         System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
@@ -30,9 +27,10 @@ public class CatalogTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://onliner.by");
-        log.info("Open onliner.by");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        logger.info("Open onliner.by");
         driver.findElement(By.cssSelector(".b-main-navigation__link")).click();
-        log.info("Click on the catalog");
+        logger.info("Click on the catalog");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
@@ -40,75 +38,75 @@ public class CatalogTest {
     public void testCheckElectronics() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Электроника"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the Еlectronics section");
+        logger.info("Checking the Еlectronics section");
     }
 
     @Test
     public void testCheckComputersAndNetworks() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Компьютеры и\u00A0сети"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the section Computers and networks");
+        logger.info("Checking the section Computers and networks");
     }
 
     @Test
     public void testCheckAppliances() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Бытовая техника"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the electronics section");
+        logger.info("Checking the electronics section");
     }
 
     @Test
     public void testCheckConstructionAndRepair() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Стройка и\u00A0ремонт"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the section Construction and repair");
+        logger.info("Checking the section Construction and repair");
     }
 
     @Test
     public void testCheckHouseAndGarden() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Дом и\u00A0сад"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the Home and Garden section");
+        logger.info("Checking the Home and Garden section");
     }
 
     @Test
     public void testCheckAutoAndMoto() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Авто и\u00A0мото"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the Auto and Moto section");
+        logger.info("Checking the Auto and Moto section");
     }
 
     @Test
     public void testCheckBeautyAndSports() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Красота и\u00A0спорт"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the section Beauty and sports");
+        logger.info("Checking the section Beauty and sports");
     }
 
     @Test
     public void testCheckForChildrenAndMothers() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Детям и\u00A0мамам"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the section for Children and mothers");
+        logger.info("Checking the section for Children and mothers");
     }
 
     @Test
     public void testCheckWorkAndOffice() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Работа и\u00A0офис"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the Work and Office section");
+        logger.info("Checking the Work and Office section");
     }
 
     @Test
     public void testCheckFood() {
         boolean expected = driver.findElement(By.xpath(String.format(PRODUCT_NAME_PATTERN, "Еда"))).isDisplayed();
         Assert.assertTrue(expected);
-        log.info("Checking the Food section");
+        logger.info("Checking the Food section");
     }
 
-    @AfterMethod
+    @AfterTest
     public void tearDown() {
         driver.quit();
-        log.info("Closing the browser");
+        logger.info("Closing the browser");
     }
 }
---
+++ src/test/java/catalog/Logger.java
@@ -0,0 +1,24 @@
+package catalog;
+
+import org.testng.Reporter;
+
+public class Logger {
+    private static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(Logger.class);
+    private static Logger instance = null;
+
+
+    private Logger() {
+    }
+
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void info(String message) {
+        logger.info(message);
+        Reporter.log(message + "<br>");
+    }
+}
